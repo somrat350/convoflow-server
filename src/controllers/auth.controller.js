c0938a44c56a2ebc760adfa6753dce484d5b3e96
somrat350 <mdsomratsordaro350@gmail.com>
2026-02-02 09:04:22 +0600

    login and logout controller implemantation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,3 +47,32 @@ export const register = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in user login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = (_, res) => {
+  res.cookie("token", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
+};
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-// Route for user register
+// Routes for user authentication
 router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
